dd23f1c64e39bf6066965da572072c1c036bab36
paruljamwal <paruljamwal2000@gmail.com>
2022-12-25 23:56:31 +0530

    find odd value using helper recursive

---
+++ helperRecursive.js
@@ -0,0 +1,19 @@
+
+
+function findOdd(array){
+    let result = [];
+    function helperRecursive(inputArray){
+       if(inputArray.length ===0){
+        return;
+       }
+       if(inputArray[0] % 2 !== 0){
+       result.push(inputArray[0]);
+       }
+       helperRecursive(inputArray.slice(1));
+    }
+    helperRecursive(array);
+    return result;
+}
+
+let res = findOdd([1,2,3,4,5,6,7,8,9,10]);
+console.log(res);
